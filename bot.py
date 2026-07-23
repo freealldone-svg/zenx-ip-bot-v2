74c8f03521bf65a5281e26211c3a3c033d8f779c
freealldone-svg <freealldone@gmail.com>
2026-07-23 10:12:49 +0600

    Step 3.2

--- bot.py
+++ bot.py
@@ -1,1 +1,22 @@
-print("ZENX IP BOT V2")
+from telegram import (
+    Update,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+)
+
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    CallbackQueryHandler,
+    MessageHandler,
+    ContextTypes,
+    filters,
+)
+
+from config import *
+from database import *
+from admin import *
+from proxy import *
+
+import json
+import os
